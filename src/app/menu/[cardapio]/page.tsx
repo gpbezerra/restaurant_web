1ce3a62a5e42ca13e228035e37d8a7af092e5c48
gpbezerra <gabrielpbezerra8@gmail.com>
2024-06-03 16:20:22 -0300

    começando

---
+++ src/app/menu/[cardapio]/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Cardapio = () => {
+    return(
+        <div>cardapios</div>
+    )
+}
+
+export default Cardapio
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const NavBar = () => {
+    return(
+        <div className="fixed bottom-0 w-full h-16 bg-white border-t border-gray-200">
+            a
+        </div>
+    )
+}
+
+export default NavBar
